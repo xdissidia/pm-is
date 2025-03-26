15a81c36cb8c7e4e3c458d62c50a7aa75b8ba057
jayfructuoso@gmail.com <jayfructuoso@gmail.com>
2025-03-26 15:49:02 +0800

    update: upgrade secure

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
 
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <title inertia>Project Management Information System</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
