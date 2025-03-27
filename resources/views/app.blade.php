586f5448b0638276e2464b7d32620daee56aae0c
root@docker.com <root@docker.com>
2025-03-27 02:26:51 +0000

    up

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,8 +10,9 @@
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+    <!-- <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> -->
+    <link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet" />
+    
     <!-- Scripts -->
     @routes()
     @viteReactRefresh
