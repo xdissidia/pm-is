a5eb040cc09e13020d479f3b771dd8c4954d39e4
jayfructuoso@gmail.com <jayfructuoso@gmail.com>
2025-04-22 11:15:15 +0800

    fix:assigned user

--- app/Actions/Task/CreateTask.php
+++ app/Actions/Task/CreateTask.php
@@ -22,7 +22,7 @@ class CreateTask
             $task = $project->tasks()->create([
                 'group_id' => $data['group_id'],
                 'created_by_user_id' => auth()->id(),
-                'assigned_users' => $data['assigned_users'],
+                // 'assigned_users' => $data['assigned_users'],
                 'name' => $data['name'],
                 'number' => $project->tasks()->withArchived()->count() + 1,
                 'description' => $data['description'],
