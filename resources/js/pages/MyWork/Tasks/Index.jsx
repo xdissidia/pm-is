3c5efd9418d78c1c42dfc2f574d15ae3df3af5b2
jay@msi.laptop <jay@msi.laptop>
2026-02-11 22:44:06 +0800

    test?

--- resources/js/pages/MyWork/Tasks/Index.jsx
+++ resources/js/pages/MyWork/Tasks/Index.jsx
@@ -57,7 +57,7 @@ const TasksIndex = () => {
 
       <Box maw={1000}>
         {projects.length ? (
-          <Accordion variant="separated" radius="md" multiple defaultValue={true}>
+          <Accordion variant="separated" radius="md" multiple defaultValue={projects.map((p) => p.id.toString())}>
             {projects.map((project) => (
               <Accordion.Item
                 key={project.id}
