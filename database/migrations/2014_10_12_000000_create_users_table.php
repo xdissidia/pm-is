5841630213a2ab2690c924a143eaa9e7f3bc5108
jay@msi.laptop <jay@msi.laptop>
2026-08-14 22:58:22 +0800

    fix: Correct syntax error in users table migration for password field

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('password')->;
+            $table->string('password');
             $table->string('avatar')->nullable();
             $table->string('phone')->nullable();
             $table->string('job_title')->nullable();
